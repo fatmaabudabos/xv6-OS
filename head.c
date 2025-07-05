83262de60b676292d437436b5c7625a7696216b4
asmaachreih1 <aas139@mail.aub.edu>
2025-07-05 23:55:22 +0300

    Final testing and fixes for head command

--- head.c
+++ head.c
@@ -11,35 +11,40 @@ int main(int argc, char *argv[]) {
   char buf[BUF_SIZE];
 
   if (argc < 2) {
-    fprintf(2, "Usage: head filename [num_lines]\n");
+    printf(1, "Usage: head filename [num_lines]\n");
     exit();
   }
 
   if (argc == 3) {
     lines = atoi(argv[2]);
     if (lines <= 0) {
-      fprintf(2, "Invalid number of lines: %s\n", argv[2]);
+      printf(1, "Invalid number of lines: %s\n", argv[2]);
       exit();
     }
   }
 
   int fd = open(argv[1], 0);
   if (fd < 0) {
-    fprintf(2, "head: cannot open %s\n", argv[1]);
+    printf(1, "head: cannot open %s\n", argv[1]);
     exit();
   }
 
   int n, line_count = 0;
   while ((n = read(fd, buf, sizeof(buf))) > 0) {
-    for (int i = 0; i < n && line_count < lines; i++) {
+    for (int i = 0; i < n; i++) {
       write(1, &buf[i], 1);
       if (buf[i] == '\n') {
         line_count++;
-        if (line_count >= lines) break;
+        if (line_count >= lines) {
+          close(fd);
+          exit();
+        }
       }
     }
-    if (line_count >= lines)
-      break;
+  }
+
+  if (n < 0) {
+    printf(1, "head: read error\n");
   }
 
   close(fd);
